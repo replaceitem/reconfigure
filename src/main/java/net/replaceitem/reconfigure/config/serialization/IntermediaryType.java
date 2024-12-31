674aba97816984972810e8994da8d33f04f246ec
replaceitem <40722305+replaceitem@users.noreply.github.com>
2024-12-31 01:31:07 +0100

    Add names to intermediary types for debugging

--- src/main/java/net/replaceitem/reconfigure/config/serialization/IntermediaryType.java
+++ src/main/java/net/replaceitem/reconfigure/config/serialization/IntermediaryType.java
@@ -1,14 +1,25 @@
 package net.replaceitem.reconfigure.config.serialization;
 
 public class IntermediaryType<M extends Intermediary<?>> {
+    public static final IntermediaryType<Intermediary.IntermediaryString> STRING = new IntermediaryType<>("string");
+    public static final IntermediaryType<Intermediary.IntermediaryInteger> INTEGER = new IntermediaryType<>("integer");
+    public static final IntermediaryType<Intermediary.IntermediaryDouble> DOUBLE = new IntermediaryType<>("double");
+    public static final IntermediaryType<Intermediary.IntermediaryBoolean> BOOLEAN = new IntermediaryType<>("boolean");
+    public static final IntermediaryType<Intermediary.IntermediaryList> LIST = new IntermediaryType<>("list");
+    
+    private final String name;
+
+    public IntermediaryType(String name) {
+        this.name = name;
+    }
+    
     @SuppressWarnings("unchecked")
     public M cast(Object o) {
         return (M) o;
     }
-    
-    public static final IntermediaryType<Intermediary.IntermediaryString> STRING = new IntermediaryType<>();
-    public static final IntermediaryType<Intermediary.IntermediaryInteger> INTEGER = new IntermediaryType<>();
-    public static final IntermediaryType<Intermediary.IntermediaryDouble> DOUBLE = new IntermediaryType<>();
-    public static final IntermediaryType<Intermediary.IntermediaryBoolean> BOOLEAN = new IntermediaryType<>();
-    public static final IntermediaryType<Intermediary.IntermediaryList> LIST = new IntermediaryType<>();
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
