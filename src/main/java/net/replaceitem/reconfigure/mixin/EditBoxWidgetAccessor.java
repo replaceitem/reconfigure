b3a4302064c82df9840a545a1a50d5a5d04556e5
replaceitem <40722305+replaceitem@users.noreply.github.com>
2025-09-01 20:52:47 +0200

    Removed mixin debug export in EditBoxWidgetAccessor

--- src/main/java/net/replaceitem/reconfigure/mixin/EditBoxWidgetAccessor.java
+++ src/main/java/net/replaceitem/reconfigure/mixin/EditBoxWidgetAccessor.java
@@ -6,7 +6,6 @@ import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Debug(export = true)
 @Mixin(EditBoxWidget.class)
 public interface EditBoxWidgetAccessor {
     @Accessor
