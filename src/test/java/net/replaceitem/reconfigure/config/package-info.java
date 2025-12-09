1d93fdac3c6128e937a9cebacb664396154d313b
replaceitem <40722305+replaceitem@users.noreply.github.com>
2025-12-09 22:46:10 +0100

    Fixed tests

---
+++ src/test/java/net/replaceitem/reconfigure/config/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package net.replaceitem.reconfigure.config;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/test/java/net/replaceitem/reconfigure/config/property/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package net.replaceitem.reconfigure.config.property;
+
+import org.jspecify.annotations.NullMarked;
